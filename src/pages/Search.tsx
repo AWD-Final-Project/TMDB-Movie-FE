e6f70da2f8fffe7d5fe6c89655e53d02e1e99ca4
ndnhattan <ndnhattan911@gmail.com>
2025-01-09 11:08:52 +0700

    Add filter

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,37 +2,49 @@ import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axiosClient from "../configs/axios";
 import { IMovie } from "./Detail";
-import { Container } from "@mui/material";
+import { Container, MenuItem, Select } from "@mui/material";
 import classNames from "classnames";
+import { SyncLoader } from "react-spinners";
 
 const Search = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
+  const language = searchParams.get("language") || "all";
+  const year = searchParams.get("year") || "all";
   const [movies, setMovies] = useState<IMovie[]>([]);
   const [searchQuery, setSearchQuery] = useState(query || "");
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const searchMovie = async () => {
       try {
-        const response = await axiosClient.get(
-          `/movie/search?key_word=${query}&page=${page}`
-        );
+        setLoading(true);
+        let queryString = `/movie/search?key_word=${query}&page=${page}`;
+        if (language !== "all") {
+          queryString += `&language=${language}`;
+        }
+        if (year !== "all") {
+          queryString += `&year=${year}`;
+        }
+        const response = await axiosClient.get(queryString);
         const data = await response.data;
-        setMovies(data.data);
+        setMovies(data.data.movies);
       } catch (error) {
         console.error("Failed to fetch movies:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     searchMovie();
-  }, [query, page]);
+  }, [query, page, language, year]);
 
   return (
     <div>
       <Container className="pb-10">
-        <div className="relative border mb-8 rounded-full mt-2">
+        <div className="relative border mb-2 rounded-full mt-2">
           <input
             type="text"
             value={searchQuery}
@@ -52,53 +64,112 @@ const Search = () => {
             Search
           </button>
         </div>
-        <div className="flex flex-col gap-5">
-          {movies.map((movie) => (
-            <div
-              key={movie.id}
-              className="border rounded-md shadow flex gap-4"
-              onClick={() => navigate(`/movie/${movie.id}`)}
+
+        <div className="mb-8 mx-4 flex gap-4">
+          <div className="flex items-center gap-2">
+            <h1 className="">Language</h1>
+            <Select
+              className=""
+              size="small"
+              value={language}
+              onChange={(e) =>
+                navigate(
+                  e.target.value === "all"
+                    ? `/search?query=${searchQuery}`
+                    : `/search?query=${searchQuery}&language=${e.target.value}`
+                )
+              }
             >
-              <img
-                src={`https://image.tmdb.org/t/p/w300_and_h450_multi_faces${movie?.poster_path}`}
-                alt=""
-                className="w-24 rounded-l-md"
-              />
-              <div className="mt-4">
-                <h1 className="font-semibold text-lg">{movie.title}</h1>
-                <span className="text-gray-500">{movie.release_date}</span>
-                <p className="mt-2">{movie.overview}</p>
-              </div>
-            </div>
-          ))}
+              <MenuItem value="all">All</MenuItem>
+              <MenuItem value="en">English</MenuItem>
+              <MenuItem value="vi">Vietnamese</MenuItem>
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <h1 className="">Year</h1>
+            <Select
+              className=""
+              size="small"
+              value={year}
+              onChange={(e) =>
+                navigate(
+                  e.target.value === "all"
+                    ? `/search?query=${searchQuery}`
+                    : `/search?query=${searchQuery}&year=${e.target.value}`
+                )
+              }
+            >
+              <MenuItem value="all">All</MenuItem>
+              {Array.from({ length: 10 }, (_, index) => (
+                <MenuItem value={new Date().getFullYear() - index}>
+                  {new Date().getFullYear() - index}
+                </MenuItem>
+              ))}
+            </Select>
+          </div>
         </div>
-        <div className="flex justify-center gap-4 mt-4">
-          <button
-            className={classNames({ hidden: page === 1 })}
-            onClick={() => setPage(page - 1)}
-          >
-            Previous
-          </button>
 
-          {[page - 2, page - 1, page, page + 1, page + 2].map((p) => {
-            if (p > 0)
-              return (
-                <button
-                  key={p}
-                  className={`${
-                    page === p ? "bg-gray-300 rounded w-6 h-6" : ""
-                  }`}
-                  onClick={() => setPage(p)}
+        {loading ? (
+          <div className="flex items-center justify-center w-full h-[350px]">
+            <SyncLoader color="#1ed5a9" />
+          </div>
+        ) : (
+          <>
+            <div className="flex flex-col gap-5">
+              {movies.map((movie) => (
+                <div
+                  key={movie.id}
+                  className="border rounded-md shadow flex gap-4"
+                  onClick={() => navigate(`/movie/${movie.id}`)}
                 >
-                  {p}
+                  <img
+                    src={`https://image.tmdb.org/t/p/w300_and_h450_multi_faces${movie?.poster_path}`}
+                    alt=""
+                    className="w-24 rounded-l-md"
+                  />
+                  <div className="mt-4">
+                    <h1 className="font-semibold text-lg">{movie.title}</h1>
+                    <span className="text-gray-500">{movie.release_date}</span>
+                    <p className="mt-2">{movie.overview}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            {movies.length > 0 ? (
+              <div className="flex justify-center gap-4 mt-4">
+                <button
+                  className={classNames({ hidden: page === 1 })}
+                  onClick={() => setPage(page - 1)}
+                >
+                  Previous
                 </button>
-              );
-          })}
 
-          <button className="" onClick={() => setPage(page + 1)}>
-            Next
-          </button>
-        </div>
+                {[page - 2, page - 1, page, page + 1, page + 2].map((p) => {
+                  if (p > 0)
+                    return (
+                      <button
+                        key={p}
+                        className={`${
+                          page === p ? "bg-gray-300 rounded w-6 h-6" : ""
+                        }`}
+                        onClick={() => setPage(p)}
+                      >
+                        {p}
+                      </button>
+                    );
+                })}
+
+                <button className="" onClick={() => setPage(page + 1)}>
+                  Next
+                </button>
+              </div>
+            ) : (
+              <div className="flex items-center justify-center w-full h-[350px]">
+                <h1 className="text-xl">No results found</h1>
+              </div>
+            )}
+          </>
+        )}
       </Container>
     </div>
   );
